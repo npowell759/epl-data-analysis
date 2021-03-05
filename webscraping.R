6e1238a2706086b71001d59960e3b4433f3ad13e
Gardenhead <npowell759@gmail.com>
2021-03-05 22:43:55 +0000

    fixed encoding issues, standardised syntax

--- webscraping.R
+++ webscraping.R
@@ -1,29 +1,31 @@
 library(rvest)
 library(dplyr)
 
-link = "https://www.transfermarkt.co.uk/premier-league/transfers/wettbewerb/GB1/plus/?saison_id=2019"
-page = read_html(link)
+link <- "https://www.transfermarkt.co.uk/premier-league/transfers/wettbewerb/GB1/plus/?saison_id=2019"
+page <- read_html(link)
 
-team = page %>% html_nodes("h2 a") %>% html_text() %>% .[21:40]
+team <- page %>% html_nodes("h2 a") %>% html_text() %>% .[21:40]
 
-expenditure = page %>% html_nodes(".transfer-einnahmen-ausgaben.redtext") %>% html_text() %>% 
-  gsub("\n\t\tExpenditure: £", "", .) %>% gsub("m\t\t\t\t\t\t", "", .) %>% as.numeric(.)
+expenditure <- page %>% html_nodes(".transfer-einnahmen-ausgaben.redtext") %>% html_text() %>% 
+  gsub("\n\t\tExpenditure: Â£", "", .) %>% gsub("m\t\t\t\t\t\t", "", .) %>% as.numeric(.)
 
-income = page %>% html_nodes(".transfer-einnahmen-ausgaben.greentext") %>% html_text() %>%
-  gsub("\n\t\t\t\t\t\tIncome: £", "", .) %>% gsub("m\t\t\t\t\t\t\t", "", .) %>%
+income <- page %>% html_nodes(".transfer-einnahmen-ausgaben.greentext") %>% html_text() %>%
+  gsub("\n\t\t\t\t\t\tIncome: Â£", "", .) %>% gsub("m\t\t\t\t\t\t\t", "", .) %>% 
   gsub("Th.\t\t\t\t\t\t\t", "", .) %>% as.numeric(.)
 
-income[19] = income[19] / 1000
+income[income>250] <- income[income>250] / 1000
+income <- round(income, digits = 2)
 
 net_spend <- income - expenditure
 
 largest_in <- page %>% html_nodes(".responsive-table:nth-child(2) tr:nth-child(1) .rechts a") %>% html_text() %>%
-  gsub("£", "", .) %>% gsub("m", "", .) %>% as.numeric(.)
+  gsub("Â£", "", .) %>% gsub("m", "", .) %>% as.numeric(.)
 
 largest_out <- page %>% html_nodes(".responsive-table:nth-child(4) tr:nth-child(1) .rechts a") %>% html_text() %>%
-  gsub("£", "", .) %>% gsub("m", "", .) %>% gsub("Loan fee:", "", .) %>% gsub("Th.", "", .) %>% as.numeric(.)
+  gsub("Â£", "", .) %>% gsub("m", "", .) %>% gsub("Loan fee:", "", .) %>% gsub("Th.", "", .) %>% as.numeric(.)
 
-largest_out[c(18, 19)] = largest_out[c(18, 19)] / 1000
+largest_out[largest_out > 140] <- largest_out[largest_out > 140] / 1000
+largest_out <- round(largest_out, digits = 2)
 
 transferspending <- data.frame(team, expenditure, income, net_spend, largest_in, largest_out, stringsAsFactors = FALSE)
 
